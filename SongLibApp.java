3f84c29666ef9b4206ca7f31de8ea2377204d42e
Gianluca Delbarba <48185234+gianlucadb6@users.noreply.github.com>
2021-02-10 17:19:35 -0500

    Add files via upload

---
+++ SongLibApp.java
@@ -0,0 +1,33 @@
+package songlib.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class SongLibApp extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/songlib/view/songlib.fxml"));
+		
+		AnchorPane root = (AnchorPane)loader.load();
+		
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Song Library");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+	
+
+}
---
+++ SongLibController.java
@@ -0,0 +1,36 @@
+package songlib.view;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+
+public class SongLibController {
+	@FXML ListView<String> listview;
+	@FXML Button add;
+	@FXML Button edit;
+	@FXML Button delete;
+	
+	private ObservableList<String> songList;
+	
+	public void start() {
+		songList = FXCollections.observableArrayList("Fear by Kendrick",
+													"10CC by Biggie", 
+													"Everybody Loves the Sunshine");
+		listview.setItems(songList);
+		
+	}
+	
+	public void convertButton(ActionEvent e) {
+		Button b = (Button)e.getSource();
+		if(b == add) {//add button was pressed
+			
+		}else if(b == edit) {//edit button was pressed
+			
+		}else {//delete button was pressed
+			
+		}
+	}
+}

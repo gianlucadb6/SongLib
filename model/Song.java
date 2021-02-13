19325bbaeca609538fd3c4a5637fcae8d0633877
geiselmanl <78880989+geiselmanl@users.noreply.github.com>
2021-02-13 13:13:49 -0500

    Update Song.java

--- model/Song.java
+++ model/Song.java
@@ -1,1 +1,25 @@
+package model;
 
+public class Song {
+
+	private String name;
+	private String artist;
+	private String album;
+	private String year;
+	
+	//Constructor 
+	public Song(String name, String artist, String album, String year){
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	
+	//Constructor for only name and artist
+	public Song(String name, String artist){
+		this(name, artist, "", "");
+	}
+
+	
+
+}

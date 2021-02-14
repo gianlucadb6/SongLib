8f065c1a466db8f8262199690689ee38f9b1919b
Gianluca Delbarba <48185234+gianlucadb6@users.noreply.github.com>
2021-02-14 14:11:59 -0500

    Update SongLibController.java
    improved add functionality, and added delete functions

--- SongLibController.java
+++ SongLibController.java
@@ -5,32 +5,78 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import model.Song;
 import javafx.event.ActionEvent;
 
 public class SongLibController {
-	@FXML ListView<String> listview;
+	@FXML ListView<Song> listView;
+	@FXML Text display;
 	@FXML Button add;
-	@FXML Button edit;
 	@FXML Button delete;
+	@FXML Button edit;
+	@FXML TextField title;
+	@FXML TextField artist;
+	@FXML TextField year;
+	@FXML TextField album;
 	
-	private ObservableList<String> songList;
+	private ObservableList<Song> obsList;
 	
-	public void start() {
-		songList = FXCollections.observableArrayList("Fear by Kendrick",
-													"10CC by Biggie", 
-													"Everybody Loves the Sunshine");
-		listview.setItems(songList);
+	public void start(Stage mainStage) {
+		Song song1 = new Song("Fear", "Kendrick");
+		Song song2 = new Song("10cc", "Biggie", "1995", "Ready to Die");
 		
+		obsList = FXCollections.observableArrayList(song1, song2);
+		listView.setItems(obsList);
+		listView.getSelectionModel().select(0);
+		display.setText(listView.getSelectionModel().getSelectedItem().toString());
+		
+		listView
+		.getSelectionModel()
+		.selectedIndexProperty()
+		.addListener(
+				(obs, oldVal, newVal) -> 
+				showSong(mainStage));
+		
+
 	}
+	/*TO DO:
+	 * alphabetical ordering of list items
+	 * list items don't show all the info, just song name
+	 * file that holds on to library info
+	 * edit option
+	 * delete option - done
+	 * empty list select(0)
+	 */
+	private void showSong(Stage mainStage) {
+		display.setText(listView.getSelectionModel().getSelectedItem().toString());
+	}
+
 	
+
 	public void convertButton(ActionEvent e) {
 		Button b = (Button)e.getSource();
-		if(b == add) {//add button was pressed
-			
-		}else if(b == edit) {//edit button was pressed
-			
-		}else {//delete button was pressed
-			
+		if(b == add) {
+			if(title.getText().equals("") || artist.getText().contentEquals("")) {
+				if(title.getText().equals("")) {
+					title.setPromptText("Enter a a valid title.");
+				}
+				if(artist.getText().contentEquals("")) {
+					artist.setPromptText("Enter a valid artist.");					
+				}
+			}else {
+				if(album.getText().equals("") && year.getText().equals("")) {
+					obsList.add(new Song(title.getText(), artist.getText()));
+				}else {
+					obsList.add(new Song(title.getText(), artist.getText(),
+							album.getText(), year.getText()));
+				}
+			}
+		}else if(b == delete) {
+			int index = listView.getSelectionModel().getSelectedIndex();
+			obsList.remove(index);
 		}
 	}
 }

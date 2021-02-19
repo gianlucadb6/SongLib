928e9b215cf4feb4633620d9aa3a302d5395517f
Gianluca Delbarba <48185234+gianlucadb6@users.noreply.github.com>
2021-02-19 14:40:24 -0500

    Update Song.java

--- Song.java
+++ Song.java
@@ -1,8 +1,5 @@
 package model;
 
-import java.io.*;
-
-import javafx.collections.*;
 
 public class Song {
 
@@ -17,21 +14,20 @@ public class Song {
 		this.name = name;
 		this.artist = artist;
 		this.album = album;
-		this.year = year;
+		if(year >= 1000 && year < 2021) {
+			this.year = year;
+		}else {
+			this.year = -1;
+		}
 	}
 
 	//Constructor for only name and artist
 	public Song(String name, String artist){
-		this(name, artist, "", 0);
+		this(name, artist, "", -1);
 	}
 
 	public String toString() {
-		if(!this.album.equals("") && this.year != 0) {
-			return "Title: " + this.name + "\nArtist: " + this.artist + 
-					"\nAlbum: " + this.album + "\nYear: " + this.year;
-		}else {
-			return "Title: " + this.name + "\nArtist: " + this.artist;
-		}
+		return this.name + ", " + this.artist;
 	}
 
 	public String getName() {
@@ -66,34 +62,13 @@ public class Song {
 		this.year = year;
 	}
 
-	public String listDisplay() {
-		return this.name + ", " + this.artist;
-	}
-
-	public void writeToFile() {
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("./src/model/library.txt"));
-			String info = this.name+", "+this.artist+", "+this.album+", "+this.year+"\n";
-			writer.write(info);
-			writer.flush();
-			writer.close();
-		}catch (IOException e){
-			System.out.println("invalid file path.");
+	public String displaySong() {
+		if(!this.album.equals("") && this.year > 0) {
+			return "Title: " + this.name + "\nArtist: " + this.artist + 
+					"\nAlbum: " + this.album + "\nYear: " + this.year;
+		}else {
+			return "Title: " + this.name + "\nArtist: " + this.artist;
 		}
 	}
 
-	public static void read(ObservableList list) {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("./src/model/library.txt"));
-			String line = reader.readLine();
-			while(line != null) {
-				String[] s = line.split(", ");
-				list.add(new Song(s[0], s[1], s[2], Integer.parseInt(s[3])));
-				line = reader.readLine();
-			}
-			reader.close();
-		}catch(IOException e) {
-			System.out.println("invalid file path");
-		}
-	}
 }
